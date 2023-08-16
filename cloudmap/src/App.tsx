203c2be930c57cccdb1388afc34cb04e33403a2e
gcolonni <giovanni.coloni@amadeus.com>
2023-08-16 16:10:17 +0200

    mock for the map

---
+++ cloudmap/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Flow from './components/flow'
+
+function App() {
+  return (
+    <div className="App">
+      <Flow/>
+    </div>
+  );
+}
+
+export default App;
